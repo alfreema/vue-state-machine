1a6c116768f7393460abdf73931da7c02f92611e
Aaron Freeman <aaron@sendthisfile.com>
2018-09-05 11:27:07 -0500

    Removed a comment that contained example code I used as a reference.

--- src/vsm-plugin.js
+++ src/vsm-plugin.js
@@ -72,17 +72,6 @@ vsmPlugin.install = function install(Vue, store, options) {
 
   }
 
-/*
-  export function transition (machine, {commit, state, dispatch}, {type, params, extState}) {
-    const nextState = machine.transition(state.state, {type, params}, extState);
-    commit(`update${machine.config.id}State`, nextState.value);
-    nextState.actions.forEach(actionKey => {
-      dispatch(actionKey, {type, params, history: nextState.history});
-    });
-  }
-*/
-
-
   // add a state machine to the store
   let addMachine = function addMachine(storeName, machine) {
     console.log('vsm-plugin:addMachine():storeName=' + storeName)
